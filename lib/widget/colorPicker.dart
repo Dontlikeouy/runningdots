f11e219960b546ea9a497d999cba7fc3f712fdb5
dontlikeouy <diyar_2017@bk.ru>
2023-03-24 18:31:09 +0300

    fix

--- lib/widget/colorPicker.dart
+++ lib/widget/colorPicker.dart
@@ -7,7 +7,7 @@ class ColorPicker extends StatefulWidget {
   @override
   State<ColorPicker> createState() => _ColorPickerState();
 }
-asd
+
 class _ColorPickerState extends State<ColorPicker> {
   double _value = 0;
   @override
